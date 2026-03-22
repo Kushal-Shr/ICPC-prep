c3313ccb0a080b3062e2b1ab71bbc3e4eeafb8e8
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-03-22 14:09:30 -0500

    bubble sort optimization

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -8,13 +8,19 @@ void bubbleSort(vector<int> &arr, int size)
 {
     for (int i = size - 1; i >= 0; i--)
     {
-        for (int j = 0; j < i - 1; j++)
+        bool isSwapped = false;
+
+        for (int j = 0; j < i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                isSwapped = true;
             }
         }
+
+        if (!isSwapped)
+            return;
     }
 }
 
