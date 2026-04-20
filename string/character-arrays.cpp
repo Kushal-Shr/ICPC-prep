394220d0fff948d64eb9585926ab51807d9443b0
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-04-19 23:18:20 -0500

    character-array

---
+++ string/character-arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[] = "Kushal";
+    cout << str << endl;
+
+    char str2[100];
+    cin.getline(str2, 100);
+    cout << str2 << endl;
+
+    return 0;
+}
