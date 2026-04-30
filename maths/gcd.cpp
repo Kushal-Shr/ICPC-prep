3dbeb98b7d46e091af16b81ce893a03d56f68606
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-04-30 14:09:56 -0500

    GCD-euclid-algo

---
+++ maths/gcd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    if (a > b)
+        return gcd(a % b, b);
+    else
+        return gcd(a, b % a);
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter a: ";
+    cin >> a;
+
+    cout << "Enter b: ";
+    cin >> b;
+
+    int ans = gcd(a, b);
+    cout << "The GCD of " << a << " and " << b << " is: " << ans << endl;
+    return 0;
+}
