74361547833fb55cca3bcbe3ea6db43aff984766
Kushal Shrestha <whotfisksl.17@gmail.com>
2026-06-14 01:28:04 -0500

    teacher-OOPS

---
+++ OOPs/teacher.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Teacher
+{
+private:
+    double salary;
+
+public:
+    string name;
+    string department;
+    string subject;
+
+    void changeDept(string newDept)
+    {
+        department = newDept;
+    }
+};
+
+int main()
+{
+    Teacher t1;
+
+    t1.name = "Kushal Shrestha";
+    t1.department = "CS";
+    t1.subject = "Data Structures";
+    t1.salary = 175000;
+
+    cout << t1.name << " has a salary of $" << t1.salary << endl;
+
+    return 0;
+}
